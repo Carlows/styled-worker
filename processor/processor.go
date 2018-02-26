375f54e90a4f7c434112f2d129e99e48c978fd12
Carlos Martinez <carlos.martinez@wizeline.com>
2018-02-26 13:34:26 -0600

    Run demo program and parse results

--- processor/processor.go
+++ processor/processor.go
@@ -2,6 +2,10 @@ package processor
 
 import (
 	"fmt"
+	"os/exec"
+	"regexp"
+
+	"github.com/satori/go.uuid"
 
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/carlows/styled-worker/utils"
@@ -23,7 +27,34 @@ func (message *MessageProcessor) Process(msg *sqs.Message) error {
 		return err
 	}
 
-	fmt.Println(contentPath, stylePath)
+	resultPath := fmt.Sprintf("temp/result-%s.png", uuid.NewV4())
+	output, err := message.runCommand(contentPath, stylePath, resultPath)
+	if err != nil {
+		return err
+	}
+
+	cmdOutput := string(output)
+	success, err := message.parseCmdOutput(cmdOutput)
+	if err != nil {
+		return err
+	}
+
+	if success {
+		// store result in S3
+	} else {
+		// update DynamoDB record with error message
+	}
 
 	return nil
+}
+
+func (message *MessageProcessor) runCommand(contentPath string, stylePath string, resultPath string) (output []byte, err error) {
+	contentParam := fmt.Sprintf("--content_image_path %s", contentPath)
+	styleParam := fmt.Sprintf("--style_image_path %s", stylePath)
+	outputParam := fmt.Sprintf("--output_image_path %s", resultPath)
+	return exec.Command("python", "demo.py", contentParam, styleParam, outputParam).Output()
+}
+
+func (message *MessageProcessor) parseCmdOutput(cmdOutput string) (match bool, err error) {
+	return regexp.MatchString("(?im)success", cmdOutput)
 }
